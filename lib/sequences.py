2feae3bec472c8b48bdbf2ae562a859e4faabdfa
Kerry Guarino <kerry.guarino@gmail.com>
2023-09-03 12:21:03 -0400

    completed testing

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    if (length.len == 0):
-        print(length.list.clear())
-    
+    fibonacci_sequence = []
 
+    if (length >= 1):
+        fibonacci_sequence.append(0)
+    if (length >= 2):
+        fibonacci_sequence.append(1)
+        for i in range(2,length):
+            fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+
+    print(fibonacci_sequence)
